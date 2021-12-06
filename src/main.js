9367c7cc08b8c3a3da56b317ee8174dde24dec9f
Kronzii88 <ezyh.arofi@gmail.com>
2021-12-06 09:22:11 +0700

    cleaning code indentation

--- src/main.js
+++ src/main.js
@@ -1,22 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import vuetify from './plugins/vuetify'
-import AOS from 'aos'
-import 'aos/dist/aos.css'
-import VueNumber from 'vue-number-animation'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import vuetify from "./plugins/vuetify";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import VueNumber from "vue-number-animation";
 
-
-Vue.config.productionTip = false
-Vue.use(VueNumber)
+Vue.config.productionTip = false;
+Vue.use(VueNumber);
 
 new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App),
+  render: (h) => h(App),
   mounted() {
-    AOS.init() 
+    AOS.init();
   },
-}).$mount('#app')
+}).$mount("#app");
